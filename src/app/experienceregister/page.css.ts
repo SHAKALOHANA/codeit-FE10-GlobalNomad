3215a82bc43f9cecad331504387bfab15b282ab3
skss6813 <dudcks6813@naver.com>
2024-12-31 05:57:48 +0900

    feat:등록페이지 ui구현
    -카테고리 및 시간 드롭다운 사용예정
    
    -소개 이미지 배너 위치 조정 작업 남음

--- src/app/experienceregister/page.css.ts
+++ src/app/experienceregister/page.css.ts
@@ -7,6 +7,7 @@ export const mainContainer = style({
   display: 'flex',
   justifyContent: 'space-between',
   margin: '30px auto',
+  backgroundColor: theme.colors.gray9,
 
   '@media': {
     [mediaQueries.tablet]: {
@@ -20,8 +21,8 @@ export const mainContainer = style({
 
 export const sideContainer = style({
   width: '792px',
-  height: '100px',
-  backgroundColor: theme.colors.blue2,
+  height: '100%',
+  backgroundColor: theme.colors.gray9,
 
   '@media': {
     [mediaQueries.tablet]: {
@@ -31,5 +32,122 @@ export const sideContainer = style({
       width: '343px',
     },
   },
+});
+
+export const contentContainer = style({
+  width: '100%',
+  height: '56px',
+  backgroundColor: theme.colors.white,
+  border: '1px solid #79747e',
+  borderRadius: '4px',
+});
+
+export const discriptionContainer = style({
+  width: '100%',
+  height: '346px',
+  backgroundColor: theme.colors.white,
+  border: '1px solid #79747e',
+  borderRadius: '4px',
+});
+
+export const dateContainer = style({
+  width: '379px',
+  height: '56px',
+  padding: '8px',
+  backgroundColor: theme.colors.white,
+  border: '1px solid #79747e',
+  borderRadius: '4px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  position: 'relative',
+
+  '@media': {
+    [mediaQueries.tablet]: {
+      width: '149px',
+    },
+    [mediaQueries.mobile]: {
+      width: '130px',
+      height: '44px',
+    },
+  },
+});
+
+export const reservationContainer = style({
+  width: '792px',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+export const addedDateWrapper = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  marginTop: '10px',
+});
+
+export const addedDateContainer = style({
+  width: '379px',
+  height: '56px',
+  backgroundColor: theme.colors.white,
+  border: '1px solid #79747e',
+  borderRadius: '4px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  '@media': {
+    [mediaQueries.tablet]: {
+      width: '149px',
+    },
+    [mediaQueries.mobile]: {
+      width: '130px',
+      height: '44px',
+    },
+  },
+});
+
+export const calendarWrapper = style({
+  border: `1px solid ${theme.colors.black}`,
+  padding: '10px',
+  position: 'absolute',
+  top: '56px',
+  backgroundColor: theme.colors.white,
+});
+
+export const line = style({
+  marginTop: '15px',
+  width: '792px',
+  border: `1px solid ${theme.colors.gray7}`,
+});
+
+export const imageButton = style({
+  width: '180px',
+  height: '180px',
+  borderRadius: '12px',
+  border: `1px dashed ${theme.colors.gray1}`,
+  backgroundColor: theme.colors.gray9,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginRight: '24px',
+});
+
+export const xButtonWrapper = style({
+  position: 'absolute',
+  top: '0',
+  right: '0',
+  backgroundColor: theme.colors.white,
+  borderRadius: '50%',
+  cursor: 'pointer',
+});
+
+export const introImageWrapper = style({
+  display: 'flex',
+  flexWrap: 'wrap',
+  gap: '24px',
 });
 
--- src/app/experienceregister/page.tsx
+++ src/app/experienceregister/page.tsx
@@ -1,15 +1,84 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
 import Header from '../../components/Header';
 import SideNavigationMenu from '../../components/SideNavigationMenu';
-import { mainContainer, sideContainer } from './page.css';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import {
+  mainContainer,
+  sideContainer,
+  contentContainer,
+  discriptionContainer,
+  dateContainer,
+  reservationContainer,
+  addedDateWrapper,
+  addedDateContainer,
+  calendarWrapper,
+  line,
+  imageButton,
+  xButtonWrapper,
+  introImageWrapper, // 새로운 클래스 추가
+} from './page.css';
 
-const experienceRegister = () => {
+const ExperienceRegister = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const [dates, setDates] = useState<string[]>([]);
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined);
+  const [isCalendarVisible, setIsCalendarVisible] = useState(false);
+  const [bannerImage, setBannerImage] = useState<File | null>(null);
+  const [introImages, setIntroImages] = useState<File[]>([]); // 소개 이미지 상태 추가
 
   const handleResize = () => {
     setIsMobile(window.innerWidth < 767);
+  };
+
+  const handleAddDate = () => {
+    if (selectedDate) {
+      const year = selectedDate.getFullYear().toString().slice(-2);
+      const month = String(selectedDate.getMonth() + 1).padStart(2, '0');
+      const day = String(selectedDate.getDate()).padStart(2, '0');
+      const formattedDate = `${year}/${month}/${day}`;
+      setDates([...dates, formattedDate]);
+      setSelectedDate(undefined);
+      setIsCalendarVisible(false);
+    }
+  };
+
+  const handleCalendarClick = () => {
+    setIsCalendarVisible(!isCalendarVisible);
+  };
+
+  const handleRemoveDate = (index: number) => {
+    setDates(dates.filter((_, idx) => idx !== index));
+  };
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setBannerImage(file);
+    }
+  };
+
+  const handleRemoveImage = () => {
+    setBannerImage(null);
+  };
+
+  const handleIntroImageChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const files = event.target.files;
+    if (files) {
+      setIntroImages((prevImages) => [
+        ...prevImages,
+        ...Array.from(files).slice(0, 4 - prevImages.length),
+      ]);
+    }
+  };
+
+  const handleRemoveIntroImage = (index: number) => {
+    setIntroImages(introImages.filter((_, idx) => idx !== index));
   };
 
   useEffect(() => {
@@ -28,11 +97,173 @@ const experienceRegister = () => {
         {!isMobile && <SideNavigationMenu />}
         <div className={sideContainer}>
           <h2>내 체험 등록</h2>
+          <input
+            className={contentContainer}
+            type="text"
+            placeholder="제목"
+            style={{ marginBottom: '20px' }}
+          />
+          <input
+            className={contentContainer}
+            type="text"
+            placeholder="카테고리"
+            style={{ marginBottom: '20px' }}
+          />
+          <input
+            className={discriptionContainer}
+            type="text"
+            placeholder="설명"
+          />
+          <h2>가격</h2>
+          <input className={contentContainer} type="text" placeholder="가격" />
+          <h2>주소</h2>
+          <input
+            className={contentContainer}
+            type="text"
+            placeholder="주소를 입력해주세요"
+          />
+          <h2>예약 가능한 시간대</h2>
+          <div className={reservationContainer}>
+            <div className={dateContainer}>
+              <p>YY/MM/DD</p>
+              <Image
+                src="../../../icons/calendar.svg"
+                alt="달력버튼"
+                width={32}
+                height={32}
+                style={{ position: 'absolute', right: '8px' }}
+                onClick={handleCalendarClick}
+              />
+              {isCalendarVisible && (
+                <div className={calendarWrapper}>
+                  <DayPicker
+                    selected={selectedDate}
+                    onDayClick={setSelectedDate}
+                  />
+                </div>
+              )}
+            </div>
+            <Image
+              src="../../../icons/plusbutton.svg"
+              alt="추가버튼"
+              width={56}
+              height={56}
+              onClick={handleAddDate}
+              style={{ cursor: 'pointer' }}
+            />
+          </div>
+          <div className={line}></div>
+          <div>
+            {dates.map((date, index) => (
+              <div key={index} className={addedDateWrapper}>
+                <div className={addedDateContainer}>{date}</div>
+                <Image
+                  src="../../../icons/minusbutton.svg"
+                  alt="빼기버튼"
+                  width={56}
+                  height={56}
+                  style={{ cursor: 'pointer' }}
+                  onClick={() => handleRemoveDate(index)}
+                />
+              </div>
+            ))}
+          </div>
+          <h2>배너 이미지</h2>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <input
+              type="file"
+              accept="image/*"
+              style={{ display: 'none' }}
+              onChange={handleImageChange}
+              id="image-upload"
+            />
+            <label htmlFor="image-upload" className={imageButton}>
+              <Image
+                src="../../../icons/imageplus.svg"
+                alt="이미지 등록"
+                width={56}
+                height={56}
+                style={{ cursor: 'pointer' }}
+              />
+              <p>이미지 등록</p>
+            </label>
+            {bannerImage && (
+              <div style={{ marginLeft: '30px', position: 'relative' }}>
+                <Image
+                  src={URL.createObjectURL(bannerImage)}
+                  alt="업로드된 이미지"
+                  width={180}
+                  height={180}
+                  style={{ objectFit: 'cover', borderRadius: '12px' }}
+                />
+                <div className={xButtonWrapper} onClick={handleRemoveImage}>
+                  <Image
+                    src="../../../icons/xbutton.svg"
+                    alt="삭제버튼"
+                    width={40}
+                    height={40}
+                    style={{ cursor: 'pointer' }}
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+          <h2>소개 이미지</h2>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <input
+              type="file"
+              accept="image/*"
+              multiple
+              style={{ display: 'none' }}
+              onChange={handleIntroImageChange}
+              id="intro-image-upload"
+            />
+            <label htmlFor="intro-image-upload" className={imageButton}>
+              <Image
+                src="../../../icons/imageplus.svg"
+                alt="이미지 등록"
+                width={56}
+                height={56}
+                style={{ cursor: 'pointer' }}
+              />
+              <p>소개 이미지 등록</p>
+            </label>
+            <div className={introImageWrapper}>
+              {introImages.map((file, index) => (
+                <div key={index} style={{ position: 'relative' }}>
+                  <Image
+                    src={URL.createObjectURL(file)}
+                    alt={`소개 이미지 ${index + 1}`}
+                    width={180}
+                    height={180}
+                    style={{ objectFit: 'cover', borderRadius: '12px' }}
+                  />
+                  <div
+                    className={xButtonWrapper}
+                    onClick={() => handleRemoveIntroImage(index)}
+                  >
+                    <Image
+                      src="../../../icons/xbutton.svg"
+                      alt="삭제버튼"
+                      width={40}
+                      height={40}
+                      style={{ cursor: 'pointer' }}
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default experienceRegister;
+export default ExperienceRegister;
 
