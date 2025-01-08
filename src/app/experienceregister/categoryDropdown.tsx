97e6e249f673562257075c5186ae883a862e23c5
YOUNG CHAN <107319232+skss6813@users.noreply.github.com>
2025-01-09 02:21:23 +0900

    feat: 내 체험 등록 드롭다운 및 버튼 활성화  (#54)
    * feat:등록버튼 ui 구현
    
    * feat: 카테고리 드롭다운 구현
    
    * feat:글자색,크기 통일
    
    * feat:시간 드롭다운 구현
    
    * feat:api post기능 구현

---
+++ src/app/experienceregister/categoryDropdown.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import React, { useState } from 'react';
+import {
+  DropDownContainer,
+  DropDownBoxWrap,
+  CategoryMenuBox,
+  ListItem,
+} from './CategoryDropDown.css';
+
+interface CategoryDropDownProps {
+  onCategorySelect: (category: string) => void;
+}
+
+const CategoryDropDown = ({ onCategorySelect }: CategoryDropDownProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selectedCategory, setSelectedCategory] = useState<string>('카테고리');
+
+  const onToggle = () => setIsOpen(!isOpen);
+
+  const onOptionClicked = (value: string) => () => {
+    setSelectedCategory(value);
+    onCategorySelect(value); // 부모 컴포넌트로 선택된 카테고리 전달
+    setIsOpen(false);
+  };
+
+  return (
+    <div style={{ position: 'relative' }}>
+      <div className={CategoryMenuBox} onClick={onToggle}>
+        <p
+          style={{
+            fontSize: '14px',
+            color: selectedCategory === '카테고리' ? '#a1a1a1' : '#000000',
+          }}
+        >
+          {selectedCategory}
+        </p>
+        <img
+          src="../../../icons/chevron_down.svg"
+          alt="Chevron Down"
+          width={24}
+          height={24}
+          style={{ cursor: 'pointer' }}
+        />
+      </div>
+
+      {isOpen && (
+        <div className={DropDownBoxWrap}>
+          <ul className={DropDownContainer}>
+            <li className={ListItem} onClick={onOptionClicked('문화예술')}>
+              문화예술
+            </li>
+            <li className={ListItem} onClick={onOptionClicked('식음료')}>
+              식음료
+            </li>
+            <li className={ListItem} onClick={onOptionClicked('스포츠')}>
+              스포츠
+            </li>
+            <li className={ListItem} onClick={onOptionClicked('투어')}>
+              투어
+            </li>
+            <li className={ListItem} onClick={onOptionClicked('관광')}>
+              관광
+            </li>
+            <li className={ListItem} onClick={onOptionClicked('웰빙')}>
+              웰빙
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CategoryDropDown;
+
