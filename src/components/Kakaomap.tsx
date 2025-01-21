080e885feb1a1850331d261cc5cdf429f7dbe041
April-37 <150885908+April-37@users.noreply.github.com>
2025-01-22 07:09:53 +0900

    feat: 지도 위에 마커 표시

--- src/components/Kakaomap.tsx
+++ src/components/Kakaomap.tsx
@@ -1,16 +1,16 @@
-import { Map } from "react-kakao-maps-sdk"
+import { Map, MapMarker } from "react-kakao-maps-sdk"
 import useKakaoLoader from "./useKakaoLoader"
+import { useEffect } from "react";
 
 export default function BasicMap() {
   useKakaoLoader()
-
+  const geocoder = new kakao.maps.services.Geocoder();
   return (
     <Map // 지도를 표시할 Container
       id="map"
       center={{
         // 지도의 중심좌표
-        lat: 33.450701,
-        lng: 126.570667,
+        lat: 33.55635, lng: 126.795841
       }}
       style={{
         // 지도의 크기
@@ -18,6 +18,7 @@ export default function BasicMap() {
         height: "500px",
       }}
       level={3} // 지도의 확대 레벨
-    />
+    ><MapMarker position={{ lat: 33.55635, lng: 126.795841 }}> </MapMarker>
+    </Map>
   )
 }
