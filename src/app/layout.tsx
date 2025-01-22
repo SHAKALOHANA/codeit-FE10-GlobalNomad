1862ff2c5d411399ed666b5f9777a401bb93e7bd
SHAKALOHANA <hoondndn@gmail.com>
2025-01-23 04:03:17 +0900

    Fix: mobile 네비 수정, 헤더 아이콘 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next';
 import { Providers } from './providers/ReactQueryProvider';
-import { ProfileProvider } from './context/ProfileContext';
+import { UserProvider } from '@/app/context/UserContext';
 import Script from 'next/script';
 import './global.css';
 import ClientLayout from '../components/ClientLayout';
 
 export const metadata: Metadata = {
-  title: '',
+  title: 'Global Nomad',
   description: '',
 };
 
@@ -19,17 +19,16 @@ export default function RootLayout({
     <html lang="ko">
       <body>
         <Providers>
-
           <Script
-          src="https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=9070eccd51c9a7ceee9493b2835e12f7&libraries=services"
-          strategy="beforeInteractive"
-        />
-          <ProfileProvider>
-          <ClientLayout>
-            {children}
-            <div id="portal" />
-          </ClientLayout>
-          </ProfileProvider>
+            src="https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=9070eccd51c9a7ceee9493b2835e12f7&libraries=services"
+            strategy="beforeInteractive"
+          />
+          <UserProvider>
+            <ClientLayout>
+              {children}
+              <div id="portal" />
+            </ClientLayout>
+          </UserProvider>
         </Providers>
       </body>
     </html>
